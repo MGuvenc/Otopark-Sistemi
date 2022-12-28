3ca4a3932a75ce9242a00e828ad9c4df5bd6ec4a
Murat GÜVENÇ <muratguvenc.py@gmail.com>
2022-12-28 12:56:13 +0300

    Update config.php

--- config.php
+++ config.php
@@ -2,11 +2,11 @@
 	$servername = "localhost";
 	$username = "root";
 	$password = "";
+	$dbname = "otopark";
 
-	$conn = new mysqli($servername, $username, $password);
+	$conn = new mysqli($servername, $username, $password, $dbname);
 
 	if ($conn->connect_error) {
 		die("Bağlantı Hatası: " . $conn->connect_error);
 	}
-	echo "Bağlantı Başarılı!";
 ?>
