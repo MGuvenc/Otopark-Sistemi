13edfda5a1de50cfd274d45c06fac04e746277f9
Murat GÜVENÇ <muratguvenc.py@gmail.com>
2023-01-04 16:49:22 +0300

    Update eleman.php

--- eleman.php
+++ eleman.php
@@ -5,7 +5,7 @@
 	date_default_timezone_set('Asia/Istanbul');
 	if(empty($_SESSION["kullanici"])){
 		header("Refresh: 0; url= index.php");
-	}else{
+	}else if(!strcmp("Patron", $_SESSION['yetki'])){
 ?>
 <!DOCTYPE html>
 <!--[if IE 8]> <html lang="tr" class="ie8"> <![endif]-->
@@ -125,4 +125,8 @@
 		</script>
 	</body>
 </html>
-<?php } ?>
+<?php 
+	}else{
+		header("Refresh: 0; url= dashboard.php");
+	}
+?>
