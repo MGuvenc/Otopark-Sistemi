219045e8b751a54ec907d75ad59172aa671ff19a
Murat GÜVENÇ <muratguvenc.py@gmail.com>
2023-01-04 17:46:35 +0300

    Update otopark.sql

--- assets/sql/otopark.sql
+++ assets/sql/otopark.sql
@@ -78,9 +78,9 @@ CREATE TABLE `user` (
 --
 
 INSERT INTO `user` (`id`, `kullanici`, `mail`, `sifre`, `yetki`) VALUES
-(1, 'user1', 'info@4m1a.com', '0192023a7bbd73250516f069df18b500', 'Admin'),
-(2, 'user2', 'user2@mail.com', '0192023a7bbd73250516f069df18b500', 'Patron'),
-(3, 'user2', 'user2@gmail.com', '0192023a7bbd73250516f069df18b500', 'Eleman');
+(1, 'user1', 'user1@4m1a.com', '0192023a7bbd73250516f069df18b500', 'Admin'),
+(2, 'user2', 'user2@4m1a.com', '0192023a7bbd73250516f069df18b500', 'Patron'),
+(3, 'user3', 'user3@4m1a.com', '0192023a7bbd73250516f069df18b500', 'Eleman');
 
 --
 -- Dökümü yapılmış tablolar için indeksler
