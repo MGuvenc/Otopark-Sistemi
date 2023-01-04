33abbe5dc988275b1b56c3bd6dd716077aa61e5f
Murat GÜVENÇ <muratguvenc.py@gmail.com>
2023-01-04 17:07:29 +0300

    Update yonetici.php

--- yonetici.php
+++ yonetici.php
@@ -5,7 +5,7 @@
 	date_default_timezone_set('Asia/Istanbul');
 	if(empty($_SESSION["kullanici"])){
 		header("Refresh: 0; url= index.php");
-	}else{
+	}else if(!strcmp("Admin", $_SESSION['yetki'])){
 ?>
 <!DOCTYPE html>
 <!--[if IE 8]> <html lang="tr" class="ie8"> <![endif]-->
@@ -108,4 +108,8 @@
 		</script>
 	</body>
 </html>
-<?php } ?>
+<?php 
+	}else{
+		header("Refresh: 0; url= dashboard.php");
+	}
+?>
