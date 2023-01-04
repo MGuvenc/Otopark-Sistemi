c8c9b7c15357545d3dcd1142efdd141f018d45e1
Murat GÜVENÇ <muratguvenc.py@gmail.com>
2023-01-04 17:16:46 +0300

    Update header.php

--- header.php
+++ header.php
@@ -1,28 +1,15 @@
 <div id="header" class="navbar navbar-inverse navbar-fixed-top">
 	<div class="navbar-inner">
 		<div class="container-fluid">
-			<a title="Anasayfa" class="brand" href="dashboard.php">Ana Sayfa</a>         
-
+			<a title="Anasayfa" class="brand" href="dashboard.php">Ana Sayfa</a>
 			<div class="top-nav">
 				<ul class="nav pull-right" id="top_menu">
 					<li class="divider-vertical hidden-phone hidden-tablet"></li>
 
 					<li class="dropdown">
-						<a href="#" class="dropdown-toggle" data-toggle="dropdown">
-							<i class="icon-wrench"></i>
-							<b class="caret"></b>
-						</a>
-						<ul class="dropdown-menu">
-							<li><a href="#"><i class="icon-cogs"></i>Ayarlar</a></li>
-						</ul>
-					</li>
-
-					<li class="divider-vertical hidden-phone hidden-tablet"></li>
-
-					<li class="dropdown">
 							<a href="#" class="dropdown-toggle" data-toggle="dropdown">
-							<i class="icon-user"></i>
-							<b class="caret"></b>
+								<i class="icon-user"></i>
+								<b class="caret"></b>
 							</a>
 						<ul class="dropdown-menu">
 							<li><a href="#"><i class="icon-user"></i> <?=$_SESSION['kullanici']?></a></li>
