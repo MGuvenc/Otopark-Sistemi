e1360ab075d8eab986ffd60b00f1488fca594183
Murat GÜVENÇ <muratguvenc.py@gmail.com>
2023-01-04 16:47:22 +0300

    Update arac_giris.php

--- arac_giris.php
+++ arac_giris.php
@@ -5,7 +5,7 @@
 	date_default_timezone_set('Asia/Istanbul');
 	if(empty($_SESSION["kullanici"])){
 		header("Refresh: 0; url= index.php");
-	}else{
+	}else if(!strcmp("Eleman", $_SESSION['yetki'])){
 ?>
 <?php
 	$mesaj = "";
@@ -125,4 +125,8 @@
 		</script>
 	</body>
 </html>
-<?php } ?>
+<?php 
+	}else{
+		header("Refresh: 0; url= dashboard.php");
+	}
+?>
